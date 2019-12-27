f61464d317e5e8df729af0b4424ce48433bc0d71
David Wheatley <davidwheatley03@gmail.com>
2019-12-27 23:41:55 +0000

    Update build directory

--- build/credentials.example.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const token = `Replace this with your token!`;
-exports.token = token;
--- build/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const main_1 = require("./src/main");
-main_1.InitialiseBot(OnBotInitialised);
-function OnBotInitialised(client) {
-    client.on("message", main_1.MessageReceivedHandler);
-}
